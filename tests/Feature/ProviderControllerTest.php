a126a43bb91ebf9d947f11c140e209e7dcf20895
refaat <refaat.aish@edigits.net>
2019-11-19 22:03:14 +0200

    apply unit testing with acceptance criteria

---
+++ tests/Feature/ProviderControllerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\DataProviderEnum;
+use App\Enums\StatusProviderEnum;
+use App\Repositories\ProviderRepository;
+use Tests\TestCase;
+
+class ProviderControllerTest extends TestCase
+{
+    private $provider_repository;
+    private $data = [];
+
+    function setUp(): void
+    {
+        parent::setUp();
+        $this->provider_repository = new ProviderRepository();
+    }
+
+    public function testAllUsers()
+    {
+        $response = $this->get('/api/v1/users');
+        $response->assertStatus(200);
+        $data = $this->provider_repository->get_users([]);
+        $response->assertJson([
+            'users' => $data
+        ]);
+    }
+
+    public function testUsersWithProviderX()
+    {
+        $response = $this->get('/api/v1/users?provider=' . DataProviderEnum::DataProviderX);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'provider' => DataProviderEnum::DataProviderX
+        ]);
+
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithProviderY()
+    {
+        $response = $this->get('/api/v1/users?provider=' . DataProviderEnum::DataProviderY);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'provider' => DataProviderEnum::DataProviderY
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithAuthorizedStatusCode()
+    {
+        $response = $this->get('/api/v1/users?statusCode=' . StatusProviderEnum::AUTHORIZED);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'statusCode' => StatusProviderEnum::AUTHORIZED
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithDeclineStatusCode()
+    {
+        $response = $this->get('/api/v1/users?statusCode=' . StatusProviderEnum::DECLINE);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'statusCode' => StatusProviderEnum::DECLINE
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithRefundedStatusCode()
+    {
+        $response = $this->get('/api/v1/users?statusCode=' . StatusProviderEnum::REFUNDED);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'statusCode' => StatusProviderEnum::REFUNDED
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithCurrency()
+    {
+        $currency = 'AED';
+        $response = $this->get('/api/v1/users?currency=' . $currency);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'currency' => $currency
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+    public function testUsersWithBalanceRange()
+    {
+        $balanceMin = 130;
+        $balanceMax = 222;
+        $response = $this->get('/api/v1/users?balanceMin=' . $balanceMin . '&balanceMax=' . $balanceMax);
+        $response->assertStatus(200);
+        $this->data = $this->provider_repository->get_users([
+            'balanceMin' => $balanceMin,
+            'balanceMax' => $balanceMax,
+        ]);
+        $response->assertJson([
+            'users' => $this->data
+        ]);
+    }
+
+}
